4ab8d260671f4f99c2f3022461132e40c2f2f36d
Fábio <fabioalmeida0896@gmail.com>
2022-05-17 20:32:53 -0300

    test: created unity test for client services

--- src/__tests__/services/clients/createClientService.spec.ts
+++ src/__tests__/services/clients/createClientService.spec.ts
@@ -1,25 +1,52 @@
-// import { AppDataSource } from "../../../data-source";
-// import CreateClientService from "../../../services/clients/createClient.service";
+import { AppDataSource } from '../../../data-source'
+import CreateClientService from '../../../services/clients/createClient.service'
+import ListAllClients from '../../../services/clients/listAllClient.service'
+import ListClient from '../../../services/clients/listClient.service'
+import {
+  ICreateClient,
+  IClient,
+} from '../../../interfaces/client'
 
-// describe('Client Services' , () => {
-//   beforeAll(async() => {
-//     await AppDataSource.initialize().catch(err => console.log(err))
-//   })
-//   afterAll(async() => {
-//     await AppDataSource.destroy().catch(err => console.log(err))
-//   })
+describe('Client Services', () => {
+  beforeAll(async () => {
+    await AppDataSource.initialize().catch((err) =>
+      console.log(err)
+    )
+  })
+  afterAll(async () => {
+    await AppDataSource.destroy().catch((err) =>
+      console.log(err)
+    )
+  })
+  const client: ICreateClient = {
+    name: 'Gorimar',
+    email: 'gorimar@mail.com',
+    phone_number: '1234567890122',
+    intention: 'comprar',
+  }
 
-//   it('Should be able to create a new client',async () => {
-//     const client = {
-//       name:'Gorimar',
-//       email:'gorimar@mail.com',
-//       phone_number:'1234567890122',
-//       password:'$hashmaluco',
-//       intention:'comprar'
-//     }
+  let clientCreated: IClient
 
-//     const newClient = await CreateClientService.execute(client)
+  it('Should be able to create a new client', async () => {
+    const newClient = await CreateClientService.execute(
+      client
+    )
 
-//     expect(newClient).toHaveProperty('id')
-//   })
-// })
+    clientCreated = newClient
+    expect(newClient).toHaveProperty('id')
+  })
+
+  it('Should return a list of clients', async () => {
+    const allClients = await ListAllClients.execute()
+
+    expect(allClients).toHaveLength(1)
+  })
+
+  it('Should return a client by id', async () => {
+    const client = await ListClient.execute(
+      clientCreated.id
+    )
+    expect(client).toBeDefined()
+    expect(client).toHaveProperty('id')
+  })
+})
