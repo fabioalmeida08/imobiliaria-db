41b853c83de5eb37861b86f63cf09ceac398c2c8
Fábio <fabioalmeida0896@gmail.com>
2022-05-17 20:34:28 -0300

    feat: created a client interface

--- src/interfaces/client/index.ts
+++ src/interfaces/client/index.ts
@@ -2,6 +2,9 @@ export interface ICreateClient {
   name:string
   email:string
   phone_number:string
-  password:string
   intention:string
+}
+
+export interface IClient extends ICreateClient{
+  id:string
 }
