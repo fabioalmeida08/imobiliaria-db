230d9683b25049abeac469094b98286b01139d48
Fábio <fabioalmeida0896@gmail.com>
2022-05-17 11:45:23 -0300

    feat: create client register validation middleware

--- src/routes/clients/index.ts
+++ src/routes/clients/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import ClientController from "../../controllers/client.controller";
+import { expressYupMiddleware } from "express-yup-middleware";
+import clientValidator from "../../validations/clients/clientValidation";
 const clientRoute = Router()
 
 clientRoute
   .route('/')
-  .get(ClientController.index)
+  .get(expressYupMiddleware({schemaValidator:clientValidator}) ,ClientController.index)
   .post(ClientController.store)
 
 clientRoute
