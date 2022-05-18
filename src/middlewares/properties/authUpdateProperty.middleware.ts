66b9ff8d39f61ddd03d0ad409f1cb85e5abba170
Guilherme <guilherme.llopreti@gmail.com>
2022-05-18 12:56:43 -0300

    feat: properties middlewares

---
+++ src/middlewares/properties/authUpdateProperty.middleware.ts
@@ -0,0 +1,60 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { AppDataSource } from "../../data-source";
+import { Agency } from "../../entities/agency.entity";
+import { Realtor } from "../../entities/realtor.entity";
+import AppError from "../../errors/appError";
+
+const authUpdatePropertyMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization;
+  const { id_property } = req.params;
+
+  if (!token) {
+    throw new AppError("Missing authorization token", 401);
+  }
+
+  const verifyToken = token.split(" ")[1];
+
+  const secret = process.env.SECRET_KEY || "default";
+
+  const decoded = verify(verifyToken, secret);
+
+  const { sub } = decoded;
+
+  const agencyRepository = AppDataSource.getRepository(Agency);
+  const agency = await agencyRepository.findOne({
+    where: {
+      id: sub as string,
+    },
+  });
+
+  if (agency) {
+    next();
+  }
+
+  const realtorRepository = AppDataSource.getRepository(Realtor);
+  const realtor = await realtorRepository.findOne({
+    where: {
+      id: sub as string,
+    },
+  });
+
+  if (!agency && !realtor) {
+    throw new AppError("Invalid token", 401);
+  }
+
+  if (!realtor?.properties_created.some(({ id }) => id === id_property)) {
+    throw new AppError(
+      "Only the responsible realtor can access this feature",
+      401
+    );
+  }
+
+  next();
+};
+
+export default authUpdatePropertyMiddleware;
--- src/routes/properties/index.ts
+++ src/routes/properties/index.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
+
 import PropertiesController from "../../controllers/properties.controller";
+
+import authCreatePropertyMiddleware from "../../middlewares/properties/authCreateProperty.middleware";
+import authDeletePropertyMiddleware from "../../middlewares/properties/authDeleteProperty.middleware";
+import authListPropertyMiddleware from "../../middlewares/properties/authListProperty.middleware";
+import authUpdatePropertyMiddleware from "../../middlewares/properties/authUpdateProperty.middleware";
 
 const propertiesRouter = Router();
 
 propertiesRouter
   .route("/")
-  .post(PropertiesController.store)
-  .get(PropertiesController.index);
+  .post(authCreatePropertyMiddleware, PropertiesController.store)
+  .get(authListPropertyMiddleware, PropertiesController.index);
 
 propertiesRouter
   .route("/:id_property")
-  .get(PropertiesController.show)
-  .patch(PropertiesController.update)
-  .delete(PropertiesController.delete);
+  .get(authListPropertyMiddleware, PropertiesController.show)
+  .patch(authUpdatePropertyMiddleware, PropertiesController.update)
+  .delete(authDeletePropertyMiddleware, PropertiesController.delete);
 
 export default propertiesRouter;
