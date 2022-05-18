49fda9d6b16c054dcbccfaa93512ca7efbc2b34d
Igo Santana <igo.santana33@gmail.com>
2022-05-18 14:11:40 -0300

    hotfix: update branch

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm'
 import 'dotenv/config'
 
-export const AppDataSource =
+/*export const AppDataSource =
   process.env.NODE_ENV === 'test'
     ? new DataSource({
         type: 'sqlite',
@@ -18,6 +18,35 @@ export const AppDataSource =
             : false,
         synchronize:
           process.env.NODE_ENV === 'dev' ? true : false,
+        logging: true,
+        entities:
+          process.env.NODE_ENV === 'production'
+            ? ['dist/src/entities/*.js']
+            : ['src/entities/*.ts'],
+        migrations:
+          process.env.NODE_ENV === 'production'
+            ? ['dist/src/migrations/*.js']
+            : ['src/migrations/*.ts'],
+      })
+*/
+
+export const AppDataSource =
+  process.env.NODE_ENV === 'test'
+    ? new DataSource({
+        type: 'postgres',
+        url: 'postgres://igo:1234@db:5432/test',
+        entities: ['src/entities/*.*'],
+        migrations: ['src/migrations/*.ts'],
+        synchronize: true,
+      })
+    : new DataSource({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === 'production'
+            ? { rejectUnauthorized: false }
+            : false,
+        synchronize: process.env.NODE_ENV === 'dev' ? true : false,
         logging: true,
         entities:
           process.env.NODE_ENV === 'production'
