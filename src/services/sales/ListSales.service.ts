5052d1bd6af0094d73d63f9abde798558d64f8b7
Daniele Calixto <danielecalixtob@gmail.com>
2022-05-18 00:26:49 -0300

    feat: create List Sales service

--- src/services/sales/ListSales.service.ts
+++ src/services/sales/ListSales.service.ts
@@ -1,1 +1,10 @@
-export default class ListSalesService {}
+import { AppDataSource } from "../../data-source";
+import { Sales } from "../../entities/sales.entity";
+
+export default class ListSalesService {
+  public static async execute(): Promise<Sales[]> {
+    const salesRepo = AppDataSource.getRepository(Sales);
+    const listOfSales = await salesRepo.find();
+    return listOfSales;
+  }
+}
