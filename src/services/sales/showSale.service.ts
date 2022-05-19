e08ca43210b00f3106539d3f5107122d22d8e06a
Daniele Calixto <danielecalixtob@gmail.com>
2022-05-19 10:57:17 -0300

    fix: dockerfile

--- src/services/sales/showSale.service.ts
+++ src/services/sales/showSale.service.ts
@@ -1,7 +1,6 @@
 import { AppDataSource } from "../../data-source"
-import AppError from "../../errors/appError"
+
 import { Sales } from "../../entities/sales.entity"
-import { Property } from "../../entities/property.entity"
 
 export default class ShowSaleService {
   public static async execute(id: string) {
