8f92b1072dd50bfb2e29a174a30e9a8a0eb1bb4d
Daniele Calixto <danielecalixtob@gmail.com>
2022-05-19 08:21:50 -0300

    fix: fixed paramsnames

--- src/interfaces/sales/index.ts
+++ src/interfaces/sales/index.ts
@@ -1,8 +1,12 @@
 export interface ICreateSale {
-  selling_value: number;
-  down_payment: number;
-  description: string;
-  realtors: string[];
-  client_buyer: string;
-  property: string;
+  selling_value: number
+  down_payment: number
+  description: string
+  realtors: string[]
+  id_client_buyer: string
+  id_property: string
+}
+
+export interface ISale extends ICreateSale {
+  property: string
 }
