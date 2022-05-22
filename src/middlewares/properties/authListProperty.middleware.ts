1c406a3935abd4876d7f4593be1390b840d5ad19
Guilherme <guilherme.llopreti@gmail.com>
2022-05-22 19:24:31 -0300

    fix: jwt secret key verification in the properties middlewares

--- src/middlewares/properties/authListProperty.middleware.ts
+++ src/middlewares/properties/authListProperty.middleware.ts
@@ -17,7 +17,7 @@ const authListPropertyMiddleware = async (
 
     const verifyToken = token?.split(" ")[1];
 
-    const secret = process.env.SECRET_KEY || "default";
+    const secret = process.env.SECRET_KEY || "c5e728ad9311059cc3c09092b6a7aca6";
 
     const decoded = jwt.verify(verifyToken as string, secret);
 
