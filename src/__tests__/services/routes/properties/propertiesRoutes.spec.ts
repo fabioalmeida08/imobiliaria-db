cdaaad0ce460eb14f22e5cd3e6837f76632b0d1f
Igo Santana <igo.santana33@gmail.com>
2022-05-23 15:10:04 -0300

    hotfix: delete on cascade image

--- src/__tests__/services/routes/properties/propertiesRoutes.spec.ts
+++ src/__tests__/services/routes/properties/propertiesRoutes.spec.ts
@@ -227,4 +227,18 @@ describe('Succes Routes', () => {
   //   });
 })*/
 
-expect(true).toBe(true)
+import { AppDataSource } from '../../../../data-source'
+
+describe('test file', () => {
+  beforeAll(async () => {
+    await AppDataSource.initialize().catch((err) => console.log(err))
+  })
+
+  afterAll(async () => {
+    await AppDataSource.dropDatabase()
+    await AppDataSource.destroy().catch((err) => console.log(err))
+  })
+  it('Should pass', () => {
+    expect(2 + 2).toBe(4)
+  })
+})
