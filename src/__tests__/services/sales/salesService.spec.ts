bab4d3738f1778a0fabcd9816fd88faf2fa3923f
chris <cbcr93@gmail.com>
2022-05-23 18:04:49 -0300

    ref: merger develop tests

--- src/__tests__/services/sales/salesService.spec.ts
+++ src/__tests__/services/sales/salesService.spec.ts
@@ -134,3 +134,20 @@ describe('Sales Services', () => {
     })
     
   }) */
+
+  
+  import { AppDataSource } from '../../../data-source'
+
+describe('test file', () => {
+  beforeAll(async () => {
+    await AppDataSource.initialize().catch((err) => console.log(err))
+  })
+
+  afterAll(async () => {
+    await AppDataSource.dropDatabase()
+    await AppDataSource.destroy().catch((err) => console.log(err))
+  })
+  it('Should pass', () => {
+    expect(2 + 2).toBe(4)
+  })
+})
