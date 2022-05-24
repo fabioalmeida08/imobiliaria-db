e69bede9a4f7170fe22b4ff9981c565d62fdeaa0
chris <cbcr93@gmail.com>
2022-05-24 18:03:58 -0300

    ref: sales routes test

--- src/__tests__/routes/sales/salesRoutes.spec.ts
+++ src/__tests__/routes/sales/salesRoutes.spec.ts
@@ -230,21 +230,3 @@ describe("Succes Routes", () => {
 
 
 });
-
-
-
-/* import { AppDataSource } from "../../../data-source";
-
-describe("test file", () => {
-  beforeAll(async () => {
-    await AppDataSource.initialize().catch((err) => console.log(err));
-  });
-
-  afterAll(async () => {
-    await AppDataSource.dropDatabase();
-    await AppDataSource.destroy().catch((err) => console.log(err));
-  });
-  it("Should pass", () => {
-    expect(2 + 2).toBe(4);
-  });
-}); */
