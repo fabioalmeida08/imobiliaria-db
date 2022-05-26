f8f27b51789b873c30a904a502e6ae5cdd3d58a1
chris <cbcr93@gmail.com>
2022-05-26 19:03:29 -0300

    ref: service throw errors

--- src/services/sales/showSale.service.ts
+++ src/services/sales/showSale.service.ts
@@ -1,11 +1,17 @@
 import { AppDataSource } from "../../data-source"
 
 import { Sales } from "../../entities/sales.entity"
+import AppError from "../../errors/appError"
 
 export default class ShowSaleService {
   public static async execute(id: string) {
     const salesRepo = AppDataSource.getRepository(Sales)
     const sale = await salesRepo.findOneBy({ id })
+    
+    if(!sale){
+      throw new AppError("Sale is not found")
+    }
+
     return sale
   }
 }
