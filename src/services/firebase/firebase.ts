e1efd86be9f6e95b2e1fabe2d04e3ccff1504ec3
Igo Santana <igo.santana33@gmail.com>
2022-05-27 14:06:08 -0300

    hotfix: string keys

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -15,9 +15,12 @@ const BUCKET = 'capstone-m4-9d18d.appspot.com'
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL
+    projectId: String(process.env.FIREBASE_PROJECT_ID),
+    private_key: String(process.env.FIREBASE_PRIVATE_KEY)?.replace(
+      /\\n/g,
+      '\n'
+    ),
+    client_email: String(process.env.FIREBASE_CLIENT_EMAIL)
   }),
   storageBucket: BUCKET
 })
