5e751db36663ac0e1a1ddd6b7144e48c382b2952
Igo Santana <igo.santana33@gmail.com>
2022-05-27 13:56:05 -0300

    hotfix: credentials firebase

--- src/config/index.ts
+++
@@ -1,3 +0,0 @@
-import * as config from './firebase-key.json';
-
-const envConfigFile = config;
--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -11,13 +11,15 @@ interface multerFile {
 
 var admin = require('firebase-admin')
 
-var serviceAccount = require('../../config/firebase-key.json')
-
 const BUCKET = 'capstone-m4-9d18d.appspot.com'
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: BUCKET,
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL
+  }),
+  storageBucket: BUCKET
 })
 
 const bucket = admin.storage().bucket()
@@ -40,8 +42,8 @@ export const uploadImage = (
 
     const stream = file.createWriteStream({
       metaData: {
-        contentType: element.mimetype,
-      },
+        contentType: element.mimetype
+      }
     })
 
     stream.on('error', (error: any) => {
